8d02529a17f4f1741ec6ca0b6acea09807373a17
Robert Simpson <rsimp.dev@gmail.com>
2016-05-03 01:13:11 -0500

    reordered componentWillMount code for better readability

--- src/components/Integral.js
+++ src/components/Integral.js
@@ -3,15 +3,10 @@ import React, { Component } from 'react';
 export default class Integral extends Component {
     //called only before initial render, use to create JSXGraph elements
     componentWillMount(){
-        //create first glider
+        //create left glider
         var X = Number(this.props.leftBound);
         var Y = this.context.functionGraph.Y(X);
         this.leftGlider = this.context.board.create('glider',[X, Y, this.context.functionGraph],{name:'A'});
-
-        //create second glider
-        X = Number(this.props.rightBound);
-        Y = this.context.functionGraph.Y(X);
-        this.rightGlider = this.context.board.create('glider',[X, Y, this.context.functionGraph],{name:'B'});
 
         //attach leftBoundOnDrag event handler property
         if (this.props.leftBoundOnDrag){
@@ -20,12 +15,18 @@ export default class Integral extends Component {
             }.bind(this));
         }
 
+        //create right glider
+        X = Number(this.props.rightBound);
+        Y = this.context.functionGraph.Y(X);
+        this.rightGlider = this.context.board.create('glider',[X, Y, this.context.functionGraph],{name:'B'});
+
         //attach rightBoundOnDrag event handler property
         if (this.props.rightBoundOnDrag){
             this.rightGlider.on('drag', function() {
                 this.props.rightBoundOnDrag(this.rightGlider);
             }.bind(this));
         }
+
 
         //create integral
         this.integral = this.context.board.create('integral',[[() => this.leftGlider.X(), () => this.rightGlider.X()],this.context.functionGraph],{label:{strokeColor:"red"}, fillOpacity:0.2});
